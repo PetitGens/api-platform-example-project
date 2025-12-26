f53f2b1ba9cac198150ce38b729faefbc7200fe4
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2025-12-26 12:09:14 +0100

    test fixtures

---
+++ tests/Factory/AuthorFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Factory;
+
+use App\Entity\Author;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Author>
+ */
+final class AuthorFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    #[\Override]
+    public static function class(): string
+    {
+        return Author::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    #[\Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    #[\Override]
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Author $author): void {})
+        ;
+    }
+}
