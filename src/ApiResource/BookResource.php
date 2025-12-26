0f62c206fb5acfabe350c799a9dd87721b486b04
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2025-12-26 12:37:46 +0100

    Return author data alongside a book

--- src/ApiResource/BookResource.php
+++ src/ApiResource/BookResource.php
@@ -7,17 +7,19 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use App\Entity\Book;
+use Symfony\Component\ObjectMapper\Attribute\Map;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ApiResource(
   shortName: 'Book',
   stateOptions: new Options(Book::class),
-  normalizationContext: ['groups' => 'book'],
+  normalizationContext: ['groups' => ['book', 'author']],
   operations: [
     new GetCollection(),
     new Get(),
   ],
 )]
+#[Map(source: Book::class)]
 class BookResource
 {
   #[Groups('book')]
@@ -25,4 +27,7 @@ class BookResource
 
   #[Groups('book')]
   public string $title;
+
+  #[Groups('book')]
+  public AuthorResource $author;
 }
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
+use App\ApiResource\AuthorResource;
 use App\Repository\AuthorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\ObjectMapper\Attribute\Map;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
+#[Map(target: AuthorResource::class)]
 class Author
 {
     #[ORM\Id]
